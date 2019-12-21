9be71bd896875e39db99b1e44212935819f4cc97
Alberto <info@androiddesdecero.com>
2019-12-21 19:03:07 +0100

    3

--- app/src/main/java/com/androiddesdecero/viewmodellivedatakotlin/ui/ViewModelActivity.kt
+++ app/src/main/java/com/androiddesdecero/viewmodellivedatakotlin/ui/ViewModelActivity.kt
@@ -2,6 +2,7 @@ package com.androiddesdecero.viewmodellivedatakotlin.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.androiddesdecero.viewmodellivedatakotlin.R
 import com.androiddesdecero.viewmodellivedatakotlin.utils.Sumar
 import kotlinx.android.synthetic.main.activity_view_model.*
@@ -12,6 +13,7 @@ class ViewModelActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("TAG1", "onCreate()")
         setContentView(R.layout.activity_view_model)
 
         setUpView();
@@ -24,5 +26,30 @@ class ViewModelActivity : AppCompatActivity() {
             resultado = Sumar.sumar(resultado)
             tvSumar.text = "$resultado"
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("TAG1", "onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("TAG1", "onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("TAG1", "onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("TAG1", "onStop()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("TAG1", "onDestroy()")
     }
 }
