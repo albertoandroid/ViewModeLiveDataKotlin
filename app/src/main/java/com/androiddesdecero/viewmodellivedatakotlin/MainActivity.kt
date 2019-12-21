60c7f87698fc22ae1276caf3e68ee4752f14deb6
Alberto <info@androiddesdecero.com>
2019-12-21 18:45:06 +0100

    2

--- app/src/main/java/com/androiddesdecero/viewmodellivedatakotlin/MainActivity.kt
+++ app/src/main/java/com/androiddesdecero/viewmodellivedatakotlin/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.androiddesdecero.viewmodellivedatakotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.androiddesdecero.viewmodellivedatakotlin.ui.ViewModelActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpView()
+    }
+
+    fun setUpView(){
+        btViewModel.setOnClickListener {
+            startActivity(Intent(applicationContext, ViewModelActivity::class.java))
+        }
+
+        /*
+        btUserViewModel.setOnClickListener {
+            startActivity(Intent(applicationContext, UserViewModelActivity::class.java)
+            )
+        }
+
+        btLiveData.setOnClickListener {
+            startActivity(Intent(applicationContext, LiveDataActivity::class.java))
+        }
+
+        btDataBinding.setOnClickListener {
+            startActivity(Intent(applicationContext, DataBindingActivity::class.java))
+        }
+
+        btDBLABinding.setOnClickListener {
+            startActivity(Intent(applicationContext, DBLDActivity::class.java))
+        }
+        */
     }
 }
