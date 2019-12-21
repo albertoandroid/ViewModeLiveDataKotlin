a8d5601087676af217afa6d48e374ec2e52b65ac
Alberto <info@androiddesdecero.com>
2019-12-21 18:55:30 +0100

    3

--- app/src/main/java/com/androiddesdecero/viewmodellivedatakotlin/ui/ViewModelActivity.kt
+++ app/src/main/java/com/androiddesdecero/viewmodellivedatakotlin/ui/ViewModelActivity.kt
@@ -3,11 +3,26 @@ package com.androiddesdecero.viewmodellivedatakotlin.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.androiddesdecero.viewmodellivedatakotlin.R
+import com.androiddesdecero.viewmodellivedatakotlin.utils.Sumar
+import kotlinx.android.synthetic.main.activity_view_model.*
 
 class ViewModelActivity : AppCompatActivity() {
+
+    var resultado: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_model)
+
+        setUpView();
+    }
+
+    fun setUpView(){
+        tvSumar.text = "$resultado"
+
+        btSumar.setOnClickListener{
+            resultado = Sumar.sumar(resultado)
+            tvSumar.text = "$resultado"
+        }
     }
 }
---
+++ app/src/main/java/com/androiddesdecero/viewmodellivedatakotlin/utils/Sumar.kt
@@ -0,0 +1,10 @@
+package com.androiddesdecero.viewmodellivedatakotlin.utils
+
+object Sumar {
+
+    fun sumar(numero: Int): Int{
+        var numero = numero
+        numero = numero + 1
+        return numero
+    }
+}
