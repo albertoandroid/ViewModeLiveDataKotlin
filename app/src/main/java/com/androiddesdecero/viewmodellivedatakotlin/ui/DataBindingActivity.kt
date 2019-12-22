fca367bd340e7a426c90e5dd1d3c552fb8914379
Alberto <info@androiddesdecero.com>
2019-12-22 12:14:09 +0100

    3

--- app/src/main/java/com/androiddesdecero/viewmodellivedatakotlin/ui/DataBindingActivity.kt
+++ app/src/main/java/com/androiddesdecero/viewmodellivedatakotlin/ui/DataBindingActivity.kt
@@ -2,7 +2,9 @@ package com.androiddesdecero.viewmodellivedatakotlin.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.androiddesdecero.viewmodellivedatakotlin.R
+import com.androiddesdecero.viewmodellivedatakotlin.databinding.ActivityDataBindingBinding
 import com.androiddesdecero.viewmodellivedatakotlin.utils.User
 
 class DataBindingActivity : AppCompatActivity() {
@@ -11,6 +13,12 @@ class DataBindingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_data_binding)
+        //setContentView(R.layout.activity_data_binding)
+
+        val binding: ActivityDataBindingBinding = DataBindingUtil.setContentView(this@DataBindingActivity,R.layout.activity_data_binding)
+
+        user = User("Alberto", "30")
+
+        binding.user = user
     }
 }
